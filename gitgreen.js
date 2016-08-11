3cc4f61f63c915f84fb17cf80aa506588ea4a08e
linuxb <948449850@qq.com>
2016-08-11 23:08:11 +0800

    update

--- gitgreen.js
+++ gitgreen.js
@@ -1,17 +1,18 @@
 const child_process = require('child_process');
 const fs = require('fs');
 
-let currentTime = (new Date().getTime() - 200 * 86400).toString().substr(0,10)
+const currentTime = new Date().getTime().toString().substr(0,10);
+const startTime = currentTime - 200 * 86400;
 const lastTime = currentTime - 365 * 24 * 60 * 60;
-
+let commitTime = startTime;
 let runner = setInterval(() => {
-  currentTime = currentTime - 86400;
-  fs.writeFile('cheat.txt', currentTime, (err) => {
+  commitTime = commitTime - 86400;
+  fs.writeFile('cheat.txt', commitTime, (err) => {
     if (err) {
       console.error(err.message);
       clearInterval(runner);
     } else {
-      child_process.exec("git add .;GIT_AUTHOR_DATE='" + currentTime + "' GIT_COMMITTER_DATE='" + currentTime + "' git commit -m 'update'; git push -u origin master;", (err) => {
+      child_process.exec("git add .;GIT_AUTHOR_DATE='" + commitTime + "' GIT_COMMITTER_DATE='" + commitTime + "' git commit -m 'update'; git push -u origin master;", (err) => {
         if (err) {
           console.error(err.message);
         } else {
@@ -20,7 +21,7 @@ let runner = setInterval(() => {
       });
     }
   });
-  if (lastTime === currentTime) {
+  if (lastTime === commitTime) {
     clearInterval(runner);
     console.log('finish !! ');
   }
