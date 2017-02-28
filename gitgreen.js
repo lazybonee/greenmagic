f6fc3fdccd836315fa9dd3ca08ebf22d9e6a7f14
linuxb <948449850@qq.com>
2017-02-28 22:24:00 +0800

    first commit

---
+++ gitgreen.js
@@ -0,0 +1,27 @@
+const child_process = require('child_process');
+const fs = require('fs');
+
+let currentTime = new Date().getTime().toString().substr(0,10);
+const lastTime = currentTime - 365 * 24 * 60 * 60;
+
+let runner = setInterval(() => {
+  currentTime = currentTime - 86400;
+  fs.write('cheat.txt', currentTime, (err) => {
+    if (err) {
+      console.error(err.message);
+      clearInterval(runner);
+    } else {
+      child_process.exec("git add .;GIT_AUTHOR_DATE='" + currentTime + "' GIT_COMMITTER_DATE='" + currentTime + "' git commit -m 'update'; git push origin master;", (err) => {
+        if (err) {
+          console.error(err.message);
+        } else {
+          console.log('Done !!');
+        }
+      });
+    }
+  });
+  if (lastTime === currentTime) {
+    clearInterval(runner);
+    console.log('finish !! ');
+  }
+}, 800);
